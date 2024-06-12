623e3d97b05041c5e931d41a9b0e9117a46b3ee9
Rebuild <admin@rebuild.moe>
2024-06-12 14:40:10 -0400

    use assert

--- test.c
+++ test.c
@@ -1,6 +1,20 @@
 #include "monkey.h"
 #include <stdio.h>
 
+#ifndef NDEBUG
+#define ASSERT(condition, message)                                           \
+    do {                                                                     \
+        if (!(condition)) {                                                  \
+            fprintf(stderr,                                                  \
+                    "Assertion `" #condition "` failed in %s line %d: %s\n", \
+                    __FILE__, __LINE__, message);                            \
+            exit(1);                                                         \
+        }                                                                    \
+    } while (0)
+#else
+#error "This test file must be run in debug mode"
+#endif
+
 __attribute__((noinline)) int plus1(int i) {
     return i + 1;
 }
@@ -10,10 +24,11 @@ __attribute__((noinline)) int plus2(int i) {
 }
 
 int main(void) {
-    printf("%d\n", plus1(3));
+    ASSERT(plus1(1) == 2, "plus1(1) should be 2");
     monkeyc_patched p = monkeyc_patch(plus1, plus2);
-    printf("%d\n", plus1(3));
+    ASSERT(plus1(1) == 3, "plus1(1) should be 3 after patching");
     monkeyc_unpatch(&p);
-    printf("%d\n", plus1(3));
+    ASSERT(plus1(1) == 2, "plus1(1) should be 2 after removing patch");
+
     return 0;
 }
--- testcpp.cpp
+++ testcpp.cpp
@@ -1,6 +1,19 @@
 #include "monkey.h"
 #include <iostream>
 
+#ifndef NDEBUG
+#define ASSERT(condition, message)                                             \
+    do {                                                                       \
+        if (!(condition)) {                                                    \
+            std::cerr << "Assertion `" #condition "` failed in " << __FILE__   \
+                      << " line " << __LINE__ << ": " << message << std::endl; \
+            std::exit(1);                                                      \
+        }                                                                      \
+    } while (false)
+#else
+#error "This test file must be run in debug mode"
+#endif
+
 __attribute__((noinline)) int plus1(int i) {
     return i + 1;
 }
@@ -23,30 +36,42 @@ int breakA(A *a) {
     return a->c + 1;
 }
 
-int main() {
-    std::cout << plus1(1) << std::endl;
+void test_normal_function() {
+    ASSERT(plus1(1) == 2, "plus1(1) should be 2");
     {
         auto g = monkeyc::patch(plus1, plus2);
-        std::cout << plus1(1) << std::endl;
+        ASSERT(plus1(1) == 3, "plus1(1) should be 3 after patching");
     }
-    std::cout << plus1(1) << std::endl;
+    ASSERT(plus1(1) == 2, "plus1(1) should be 2 after removing patch");
+}
 
-    std::cout << "-----" << std::endl;
-
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "UnreachableCode"
+void test_member_function() {
     A a;
-    std::cout << a.a() << std::endl;
+    ASSERT(a.a() == 3, "a.a() should be 3");
     {
         auto g = monkeyc::patch(&A::a, breakA);
-        std::cout << a.a() << std::endl;
+        ASSERT(a.a() == 4, "a.a() should be 4 after patching");
+        a.c = 5;
+        ASSERT(a.a() == 6, "a.a() should be 6 after modifying member var");
     }
-    std::cout << a.a() << std::endl;
+    ASSERT(a.a() == 5, "a.a() should be 5 after removing patch");
+}
+#pragma clang diagnostic pop
 
-    std::cout << "-----" << std::endl;
-
-    std::cout << plus1(1) << std::endl;
+void test_lambda() {
+    ASSERT(plus1(1) == 2, "plus1(1) should be 2");
     {
-        auto g = monkeyc::patch(&plus1, [](int i) { return i + 114514; });
-        std::cout << plus1(1) << std::endl;
+        auto g = monkeyc::patch(&plus1, [](int i) { return i + 2; });
+        ASSERT(plus1(1) == 3, "plus1(1) should be 3 after patching");
     }
-    std::cout << plus1(1) << std::endl;
+    ASSERT(plus1(1) == 2, "plus1(1) should be 2 after removing patch");
+}
+
+int main() {
+    test_normal_function();
+    test_member_function();
+    test_lambda();
+    return 0;
 }
